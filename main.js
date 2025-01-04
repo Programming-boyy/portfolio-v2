e57ccfd1f97738623a9f4ee98277b9eba0662755
Mohammed Waleed <130412952+Programming-boyy@users.noreply.github.com>
2025-01-04 21:47:48 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// Menu Box
+function onMenuClick() {
+  var navbar = document.getElementById("navigation-bar");
+  var responsive_class_name = "responsive";
+
+  navbar.classList.toggle(responsive_class_name);
+}
+
+// WowJs
+new WOW().init();
